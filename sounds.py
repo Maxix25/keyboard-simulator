8921fcd3cc3b800c7100acc0fd1c716b0af7ceec
Maximiliano <maximiliano.besoain@gmail.com>
2022-03-10 22:32:05 -0300

    Fix path issue

--- sounds.py
+++ sounds.py
@@ -4,6 +4,7 @@ from argparse import ArgumentParser
 parser = ArgumentParser(description = "Simulate the sounds of a mechanical keyboard using a simple command line interface!")
 parser.add_argument("-s", "--sound", help = "Sound to reproduce when a key is pressed")
 parser.add_argument("-v", "--volume", help = "Volume to play the sound, default is 0.5")
+sounds_path = os.path.dirname(os.path.realpath(__file__))
 args = parser.parse_args()
 if args.sound:
 	sound_name = args.sound
@@ -16,14 +17,14 @@ else:
 if sound_name == "sp_cmxb_mp3":
 	volume = "0.13"
 try:
-	alpha_down_sounds = os.listdir(__file__[:-9] + f"/{sound_name}/alpha/down")
-	alpha_up_sounds = os.listdir(__file__[:-9] + f"/{sound_name}/alpha/up")
-	space_down_sounds = os.listdir(__file__[:-9] + f"/{sound_name}/space/down")
-	space_up_sounds = os.listdir(__file__[:-9] + f"/{sound_name}/space/up")
-	alt_down_sounds = os.listdir(__file__[:-9] + f"/{sound_name}/alt/down")
-	alt_up_sounds = os.listdir(__file__[:-9] + f"/{sound_name}/alt/up")
-	enter_down_sounds = os.listdir(__file__[:-9] + f"/{sound_name}/enter/down")
-	enter_up_sounds = os.listdir(__file__[:-9] + f"/{sound_name}/enter/up")
+	alpha_down_sounds = os.listdir(sounds_path + f"/{sound_name}/alpha/down")
+	alpha_up_sounds = os.listdir(sounds_path + f"/{sound_name}/alpha/up")
+	space_down_sounds = os.listdir(sounds_path + f"/{sound_name}/space/down")
+	space_up_sounds = os.listdir(sounds_path + f"/{sound_name}/space/up")
+	alt_down_sounds = os.listdir(sounds_path + f"/{sound_name}/alt/down")
+	alt_up_sounds = os.listdir(sounds_path + f"/{sound_name}/alt/up")
+	enter_down_sounds = os.listdir(sounds_path + f"/{sound_name}/enter/down")
+	enter_up_sounds = os.listdir(sounds_path + f"/{sound_name}/enter/up")
 except FileNotFoundError:
 	print("The sound name you specified isn't valid, please double check the name entered")
 	exit()
